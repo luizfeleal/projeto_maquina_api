04ebfed0aad33a5862c9ebd2d3d71d1e98ca0dba
Luiz Felipe <felipelearaujo@gmail.com>
2024-03-11 12:21:59 -0300

    Criação das configurações iniciais e criação das função de regras para validação e mensagens de retorno

--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Clientes extends Model
 {
     use HasFactory;
+    protected $table = "clientes";
+    protected $primaryKey = "id_cliente";
+    public $timestamps = false;
+    protected $fillable = [
+        "cliente_nome",
+        "cliente_celular",
+        "cliente_email",
+        "cliente_cpf_cnpj"
+    ];
+     public static function rules($id = null)
+     {
+        return [
+            "cliente_nome" => "required|string|max:255",
+            "cliente_celular"=> "required|string|max:15",
+            "cliente_email"=> "required|string|max:255",
+            "cliente_cpf_cnpj"=>"required|max:20|unique:clientes"
+        ];
+     }
+
+     public static function feedback($id = null)
+     {
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+            'max' => 'O campo :attribute não pode ter mais de :max caracteres.',
+            'unique' => 'O valor informado para o campo :attribute já está em uso.',
+            'min' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+        ];
+     }
 }
