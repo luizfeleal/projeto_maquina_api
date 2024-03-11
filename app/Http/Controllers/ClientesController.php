16cdd9aa11423726d4bf2f447827cdff427ab1a3
Luiz Felipe <felipelearaujo@gmail.com>
2024-03-11 12:10:11 -0300

    Criação do controller de Clientes

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try{
+            $clientes = Clientes::all();
+
+            return response()->json($clientes, 200);
+        }catch(Exception $e){
+            return response()->json(500, 'Houve um erro ao tentar coletar os clientes.');
+        }
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $dados = $request->all();
+            $validator = Validator::make($dados, Clientes::rules(), Clientes::feedback());
+            //$validatedData = $request->validate((new Usuarios)->rules(), (new Usuarios)->feedback());
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 400);
+            }
+
+            return DB::transaction(function () use ($dados) {
+                $clientes = new Clientes();
+                $clientes->fill($dados);
+                $clientes->save();
+                return response()->json(['message' => 'Cliente cadastrado com sucesso!'], 200);
+            });
+
+        } catch (ValidationException $e) {
+            return response()->json(['message' => 'Erro de validação: ' . $e->getMessage()], 422);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Houve um erro ao tentar cadastrar o cliente.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $cliente = CLiente::find($id);
+
+            if(!$cliente) {
+                return response()->json(["response" => "Cliente não encontrado"], 404);
+            }
+
+            return response()->json($cliente, 200);
+        } catch(\Exception $e) {
+            return response()->json(["response" => "Houve um erro ao tentar coletar o cliente de id: $id.", "error" => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+
+            $dados = $request->all();
+            $validator = Validator::make($dados, Clientes::rules(), CLientes::feedback());
+            //$validatedData = $request->validate((new Usuarios)->rules(), (new Usuarios)->feedback());
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 400);
+            }
+            return DB::transaction(function() use ($request, $id){
+                $cliente = Usuarios::findOrFail($id);
+
+                $cliente->fill($request);
+                $cliente->save();
+
+                return $cliente;
+            });
+        }catch(\Exception $e) {
+            return response()->json(["response" => "Houve um erro ao tentar atualizar o cliente de id: $id.", "error" => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
