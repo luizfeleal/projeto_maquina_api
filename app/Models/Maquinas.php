050924cca1423f1c1f00af5c2d855839531a61cf
Luiz Felipe <felipelearaujo@gmail.com>
2024-03-12 09:52:16 -0300

    Criação do model maquinas com funções de regras e mensagens

---
+++ app/Models/Maquinas.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Maquinas extends Model
+{
+    protected $table = 'maquinas';
+    protected $primaryKey = 'id_maquina';
+    public $timestamps = false;
+    protected $fillable = [
+        'maquina_referencia',
+        'maquina_nome',
+        'maquina_status',
+        'maquina_ultimo_contato',
+    ];
+
+    public static function rules($id = null)
+     {
+        return [
+            "maquina_referencia" => "required|string|max:255",
+            "maquina_nome"=> "required|string|max:255",
+            "maquina_status"=> "required|boolean",
+        ];
+     }
+
+     public static function feedback($id = null)
+     {
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+            'max' => 'O campo :attribute não pode ter mais de :max caracteres.',
+            'unique' => 'O valor informado para o campo :attribute já está em uso.',
+            'min' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+        ];
+     }
+}
