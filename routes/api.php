16995503e5720977fbc95f0fcc641b4e057dd762
Luiz Felipe <felipelearaujo@gmail.com>
2024-07-05 20:46:04 -0300

    criacao de rota para criar usuario para autenticar

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,12 @@ use Illuminate\Support\Facades\Hash;
 */
 
 Route::post('auth/login', 'App\Http\Controllers\AuthController@login');
-Route::post('/register', function (Request $request) {
+Route::get('/register', function (Request $request) {
     // Validação dos dados recebidos (opcional)
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:6',
-    ]);
+    
 
     // Criação do usuário
-    $user = User::create([
+    return $user = User::create([
         'name' => $request->name,
         'email' => $request->email,
         'password' => Hash::make($request->password),
