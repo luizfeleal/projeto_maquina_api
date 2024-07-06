8f7647786fac8b1ed0d918896fab54e8361ec78a
Luiz Felipe <felipelearaujo@gmail.com>
2024-07-05 22:33:16 -0300

    criacao de rota para criar usuario para autenticar

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::group([
 
 ], function ($router) {
 
-    Route::get('login', 'app\Http\Controllers\AuthController@login');
+    Route::get('login', 'App\Http\Controllers\AuthController@login');
 
 
 });
