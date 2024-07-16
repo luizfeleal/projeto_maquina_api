bd3af16ea42fcc809e1f79c89957c63cdb4492d5
Luiz Felipe <felipelearaujo@gmail.com>
2024-07-15 23:07:17 -0300

    Subindo correcoes

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -58,7 +58,6 @@ class QrController extends Controller
 
             $coletarChavePix = ChavePix::where('id_cliente', $id_cliente)->get();
 
-            return $coletarChavePix;
             if(empty($coletarChavePix)){
                 $token = AuthService::coletarToken();
                 $criarChavePix = ChaveAleatoriaService::criarChaveAleatoria($id_cliente, $token);
