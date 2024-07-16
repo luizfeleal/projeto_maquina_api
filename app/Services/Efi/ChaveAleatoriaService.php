b976dd48f43db2da7da0ed4c7c712969774f72eb
Luiz Felipe <felipelearaujo@gmail.com>
2024-07-16 18:07:25 -0300

    Subindo correcoes

--- app/Services/Efi/ChaveAleatoriaService.php
+++ app/Services/Efi/ChaveAleatoriaService.php
@@ -54,4 +54,50 @@ class ChaveAleatoriaService
         return $resposta;
         
     }
+    public static function coletarChaveAleatoria($id_cliente, string $token)
+    {
+
+        $arquivo = "Certificados/Naise/homologacaoTeste_cert.pem";
+
+        // Obtém o caminho absoluto do arquivo de certificado
+        $certificado = Storage::disk('local')->path($arquivo);
+
+        // Verifique se o arquivo realmente existe
+        if (!file_exists($certificado)) {
+            throw new \Exception("O arquivo de certificado não foi encontrado: " . $certificado);
+        }
+
+        $url = env('URL_EFI') . "/v2/gn/evp";
+
+        // Inicializa a sessão cURL
+        $ch = curl_init($url);
+
+
+        curl_setopt_array(
+            $ch,
+            array(
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => ['Accept: application/json', 'Content-Type: application/json', 'Authorization: Bearer ' . $token],
+                CURLOPT_SSL_VERIFYPEER => true, // Verifica o certificado do servidor
+                CURLOPT_SSL_VERIFYHOST => 2, // Verifica o host do certificado
+                CURLOPT_SSLCERT => $certificado // Define o certificado a ser usado
+            )
+        );
+
+        $result = curl_exec($ch);
+
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        
+        if (curl_errno($ch)) {
+            throw new \Exception("Erro durante a requisição cURL: " . curl_error($ch));
+        }
+        curl_close($ch);
+
+        $resposta = json_decode($result);
+
+        return $resposta;
+        
+    }
 }
