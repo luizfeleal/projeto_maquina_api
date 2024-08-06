e452db3e29a83365b9b5c9e5eb59f4b283f2870d
Luiz Felipe <felipelearaujo@gmail.com>
2024-08-06 15:58:47 -0300

    Criação do softdelete para maquinas e qrcode

--- app/Http/Controllers/MaquinasController.php
+++ app/Http/Controllers/MaquinasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Maquinas;
+use App\Models\QrCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
