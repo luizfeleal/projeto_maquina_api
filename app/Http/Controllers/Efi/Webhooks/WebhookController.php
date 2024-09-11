3e68f77a69c18bfba6ce26d6af4b1bcd71baf197
Luiz Felipe <felipelearaujo@gmail.com>
2024-09-11 16:27:52 -0300

    correção de erros

--- app/Http/Controllers/Efi/Webhooks/WebhookController.php
+++ app/Http/Controllers/Efi/Webhooks/WebhookController.php
@@ -140,7 +140,7 @@ class WebhookController extends Controller
 
                     $dadoCredDescriptografado = DescriptografaCredService::descriptografarCred($cred_api_pix);
                     $token = AuthEfiService::coletarToken($dadoCredDescriptografado);
-                    $id_transacao = ExtratoMaquina::where('id_maquina', $id_maquina)->where('id_end_to_end', $idE2E);
+                    $id_transacao = ExtratoMaquina::where('id_maquina', $id_maquina)->where('id_end_to_end', $idE2E)->get()->toArray()[0]['id_extrato_maquina'];
                     $devolucao = GestaoPixService::solicitarDevolucao($token, $idE2E, $id_transacao, $valor, $dadoCredDescriptografado['caminho_certificado']);
 
                     return $devolucao;
