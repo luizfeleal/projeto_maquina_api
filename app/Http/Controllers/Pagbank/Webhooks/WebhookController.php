6d935a8528fada67fc364b2dbb01de1f1335bd8f
Luiz Felipe <felipelearaujo@gmail.com>
2024-11-23 14:26:03 -0300

    modificações seguindo o trello

--- app/Http/Controllers/Pagbank/Webhooks/WebhookController.php
+++ app/Http/Controllers/Pagbank/Webhooks/WebhookController.php
@@ -60,6 +60,7 @@ class WebhookController extends Controller
             $notificacao['resposta']['credito']['id_maquina'] = $id_maquina;
             $notificacao['resposta']['debito']['id_maquina'] = $id_maquina;
 
+            return $device;
             if($device[0]['status'] != 1){
                 $liberarJogada = false;
                 Logs::create([
