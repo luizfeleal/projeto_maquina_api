47f9cee27a92c542b81cfb5397c9365c5e8d4a3e
Luiz Felipe <felipelearaujo@gmail.com>
2024-12-17 21:29:37 -0300

    testando

--- app/Http/Controllers/ExtratoMaquinaController.php
+++ app/Http/Controllers/ExtratoMaquinaController.php
@@ -316,8 +316,8 @@ class ExtratoMaquinaController extends Controller
             // Responder no formato esperado pelo DataTables
             return response()->json([
                 'data' => $extrato,
-                'recordsTotal' => count($extrato),
-                'recordsFiltered' => count($extrato)
+                'recordsTotal' => $totalRecords,
+                'recordsFiltered' => $totalRecords
             ], 200);
         } catch (Exception $e) {
             return response()->json(['error' => 'Houve um erro ao tentar coletar o extrato.'], 500);
