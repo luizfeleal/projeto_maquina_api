22eb50f726775649630727ff11c757716d69504f
Luiz Felipe <felipelearaujo@gmail.com>
2025-01-14 21:46:31 -0300

    Corrigindo a quantidade de caracteres do TXID

--- app/Http/Controllers/ExtratoMaquinaController.php
+++ app/Http/Controllers/ExtratoMaquinaController.php
@@ -641,6 +641,7 @@ public static function acumulatedPerMachineOfClient(Request $request)
             // Converte para o formato 'Y-m-d 00:00:00' para comparar com a data do banco
             $dataInicioFormatada = \Carbon::createFromFormat('Y-m-d', $dataInicio)->startOfDay()->format('Y-m-d H:i:s');
            
+            return response()->json($dataFimFormatada, 200);
             $query->where('extrato_maquina.data_criacao', '>=', $dataInicioFormatada);
         }
         
