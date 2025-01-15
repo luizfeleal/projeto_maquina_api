6695b95926fd06c65591986d8b5bc58241d5d7e7
Luiz Felipe <felipelearaujo@gmail.com>
2025-01-14 21:23:55 -0300

    Corrigindo a quantidade de caracteres do TXID

--- app/Http/Controllers/ExtratoMaquinaController.php
+++ app/Http/Controllers/ExtratoMaquinaController.php
@@ -637,6 +637,7 @@ public static function acumulatedPerMachineOfClient(Request $request)
             $query->where('extrato_maquina.extrato_operacao_tipo', $tipoTransacao);
         }
     
+        return $dataInicio;
         if ($dataInicio) {
 
             // Converte para o formato 'Y-m-d 00:00:00' para comparar com a data do banco
